c6af401b2969f35725b48aef4d8c0af97902049c
Gabriel Malosto <professional@gabdumal.com>
2024-11-15 15:35:07 -0300

    Style App with CSS module

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,6 +1,8 @@
+import styles from "./App.module.css";
+
 export default function App() {
   return (
-    <article>
+    <article className={styles.container}>
       <h1>Idle Clicker</h1>
       <p>
         Vamos construir um jogo de clicker simples para entendermos os conceitos
